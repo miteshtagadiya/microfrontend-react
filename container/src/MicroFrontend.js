2935c8e0e95f722c6ea0237e3d1c65edcc25e656
mitesh <miteshtagadiya@gmail.com>
2020-03-21 10:13:55 +0530

    MicroFrontend added in container

---
+++ container/src/MicroFrontend.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class MicroFrontend extends React.Component {
+  componentDidMount() {
+    const { name, host, document } = this.props;
+    const scriptId = `micro-frontend-script-${name}`;
+
+    if (document.getElementById(scriptId)) {
+      this.renderMicroFrontend();
+      return;
+    }
+
+    fetch(`${host}/asset-manifest.json`)
+      .then(res => res.json())
+      .then(manifest => {
+        const script = document.createElement('script');
+        script.id = scriptId;
+        script.crossOrigin = '';
+        script.src = `${host}${manifest['files']['main.js']}`;
+        script.onload = this.renderMicroFrontend;
+        document.head.appendChild(script);
+      });
+  }
+
+  componentWillUnmount() {
+    const { name, window } = this.props;
+
+    window[`unmount${name}`] && window[`unmount${name}`](`${name}-container`);
+  }
+
+  renderMicroFrontend = () => {
+    const { name, window, history } = this.props;
+
+    window[`render${name}`] && window[`render${name}`](`${name}-container`, history);
+  };
+
+  render() {
+    return <main id={`${this.props.name}-container`} />;
+  }
+}
+
+MicroFrontend.defaultProps = {
+  document,
+  window,
+};
+
+export default MicroFrontend;
