1b69578ed3367299e50f8a68ac75d9ab0f554389
mitesh <miteshtagadiya@gmail.com>
2020-03-21 10:13:35 +0530

    Header and navigation added in container

--- container/src/App.js
+++ container/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { NavLink, BrowserRouter, Route, Switch } from "react-router-dom";
+import MicroFrontend from "./MicroFrontend";
 
-function App() {
+const {
+  REACT_APP_SUBAPP1_HOST: subapp1,
+  REACT_APP_SUBAPP2_HOST: subapp2
+} = process.env;
+
+const SubApp2 = ({ history }) => (
+  <MicroFrontend history={history} host={subapp2} name="subapp2" />
+);
+
+const SubApp1 = ({ history }) => (
+  <MicroFrontend history={history} host={subapp1} name="subapp1" />
+);
+
+const Home = () => (
+  <>
+    <p>Rendered by Container</p>
+  </>
+);
+
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div style={{ padding: 25, display: "flex" }}>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20
+            }}
+            to="/home"
+          >
+            Home
+          </NavLink>
+        </div>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20
+            }}
+            to="/subapp1"
+          >
+            SubApp1
+          </NavLink>
+        </div>
+        <div style={{ padding: "0px 15px" }}>
+          <NavLink
+            style={{
+              textDecoration: "none",
+              fontWeight: "bold",
+              color: "#282c34",
+              fontSize: 20
+            }}
+            to="/subapp2"
+          >
+            SubApp2
+          </NavLink>
+        </div>
+      </div>
+
+      <Switch>
+        <Route path="/home" component={Home} />
+        <Route path="/subapp1" render={() => <SubApp1 />} />
+        <Route path="/subapp2" render={() => <SubApp2 />} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
