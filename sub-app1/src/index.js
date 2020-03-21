d09960b11d03852a583325e22f98de8b6d3434eb
mitesh <miteshtagadiya@gmail.com>
2020-03-21 10:15:06 +0530

    render and unmount function added in subapp1 and subapp2

--- sub-app1/src/index.js
+++ sub-app1/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// render micro frontend function
+window.rendersubapp1 = (containerId, history) => {
+  ReactDOM.render(
+    <App history={history} />,
+    document.getElementById(containerId)
+  );
+  serviceWorker.unregister();
+};
+
+// unmount micro frontend function
+window.unmountsubapp1 = containerId => {
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
+
+// Mount to root if it is not a micro frontend
+if (!document.getElementById("subapp1-container")) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
