860106688219095b099901efb5c262b1b9120ce8
mitesh <miteshtagadiya@gmail.com>
2020-03-21 10:12:11 +0530

    config-overrides added

---
+++ sub-app2/config-overrides.js
@@ -0,0 +1,11 @@
+module.exports = {
+  webpack: (config, env) => {
+    config.optimization.runtimeChunk = false;
+    config.optimization.splitChunks = {
+      cacheGroups: {
+        default: false,
+      },
+    };
+    return config;
+  },
+};
