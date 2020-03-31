c795da6f9c3688041f1a36ad9509c07ebc136fb2
mitesh <miteshtagadiya@gmail.com>
2020-03-31 19:35:50 +0530

    MicroFrontend.js updated

--- container/src/MicroFrontend.js
+++ container/src/MicroFrontend.js
@@ -13,12 +13,37 @@ class MicroFrontend extends React.Component {
     fetch(`${host}/asset-manifest.json`)
       .then(res => res.json())
       .then(manifest => {
+        manifest["entrypoints"].map((entry => {
+          if (typeof manifest["files"][entry] !== "undefined" && manifest["files"][entry] !== "undefined") {
+            if (entry.endsWith('.css')) {
+              const link = document.createElement('link');
+              link.id = scriptId;
+              link.href = `${process.env.NODE_ENV === "production" ? host.slice(0, host.lastIndexOf('/')) : host}${manifest["files"][entry]}`;
+              link.onload = this.renderMicroFrontend;
+              link.rel = "stylesheet"
+              document.head.appendChild(link);
+            }
+            const script = document.createElement('script');
+            script.id = scriptId;
+            script.crossOrigin = '';
+            script.src = `${process.env.NODE_ENV === "production" ? host.slice(0, host.lastIndexOf('/')) : host}${manifest["files"][entry]}`;
+            script.onload = this.renderMicroFrontend;
+            document.head.appendChild(script);
+          }
+        })
+        )
         const script = document.createElement('script');
         script.id = scriptId;
         script.crossOrigin = '';
-        script.src = `${host}${manifest['files']['main.js']}`;
+        script.src = `${process.env.NODE_ENV === "production" ? host.slice(0, host.lastIndexOf('/')) : host}${manifest["files"]["main.js"]}`;
         script.onload = this.renderMicroFrontend;
         document.head.appendChild(script);
+        const link = document.createElement('link');
+        link.id = scriptId;
+        link.href = `${process.env.NODE_ENV === "production" ? host.slice(0, host.lastIndexOf('/')) : host}${manifest["files"]["main.css"]}`;
+        link.onload = this.renderMicroFrontend;
+        link.rel = "stylesheet"
+        document.head.appendChild(link);
       });
   }
 
